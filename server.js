7e6bc149e449252918b5a1fde639fa009f41dc52
MarkoMesel <mmarkom0001@gmail.com>
2025-01-11 12:55:53 +0100

    installed cors;  implemented app.get method in server.js so that it can send the data stored in db.json to the angular application

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-// Import the Express package
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+// Cors configuration - Allows requests from localhost:4200
+const corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 204,
+  methods: "GET",
+};
 
 // Create an Express application
 const app = express();
 
+// Use cors middleware
+app.use(cors(corsOptions));
+
 // Define a simple route
-app.get('/', (req, res) => {
-  res.send('Test!');
+app.get("/products", (req, res) => {
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    const result = jsonData.items.slice(start, end);
+
+    res
+      .status(200)
+      .json({
+      items: result
+    });
+  });
 });
 
 // Set the server to listen on port 3000
